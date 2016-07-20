4577831cb99cf6f40cf6b1d6fd0e7e3c240b0125
jiabin <jeremy.wangjb@gmail.com>
2016-07-20 13:18:55 -0400

    test for image loading

--- GLCMproj/src/calGLCM/loadImage.java
+++ GLCMproj/src/calGLCM/loadImage.java
@@ -11,6 +11,7 @@ public class loadImage {
 	BufferedImage img;
 	File imageFile;
 	int[][] grayLevelMatrix;
+	int[] testPixel;
 	
 	loadImage (File file){
 		this.imageFile = file;
@@ -25,6 +26,7 @@ public class loadImage {
 		try {
 			img = ImageIO.read(file);
 			grayLevelMatrix = imageToGray (img);
+			testPixel = testPixel(img);
 			
 		} catch (IOException e) {
 			e.printStackTrace();
@@ -47,10 +49,23 @@ public class loadImage {
 			for (int w = 0; w < width; w ++){
 				Color c = new Color (image.getRGB(w, h));
 				
-				result[h][w] = (int) (c.getRed() * 0.299) + (int)(c.getGreen() * 0.587) + (int)(c.getBlue() * 0.114);
+				result[h][w] = (int) (c.getRed() * 0.2989) + (int)(c.getGreen() * 0.5870) + (int)(c.getBlue() * 0.114);
 				//result[h][w] = (c.getRed() + c.getGreen() + c.getBlue()) / 3;
 			}
 		}
+		
+		return result;
+	}
+	
+	private int[] testPixel (BufferedImage image){
+		
+		int[] result = new int[3];
+		
+		Color c = new Color (image.getRGB(0, 39));
+		
+		result[0] = c.getRed();
+		result[1] = c.getGreen();
+		result[2] = c.getBlue();
 		
 		return result;
 	}
@@ -59,5 +74,29 @@ public class loadImage {
 		return grayLevelMatrix;
 	}
 	
+	public int[] getRGBvalue (){
+		return testPixel;		
+	}
 	
+	public static void main(String[] args) {
+		
+		File loadingFile = new File ("C:/Users/jiabin/Desktop/GLCM_Cal/Image/45deg/camera0/PicA1009.bmp");
+		
+		loadImage lI = new loadImage (loadingFile);
+		
+		int[][] tempResult = lI.getGrayLevelMatrix();
+		int[] testResult = lI.getRGBvalue();
+		
+		for (int row = 40; row < 50; row ++){
+			for (int column = 0; column < tempResult[row].length; column ++){
+				System.out.print(tempResult[row][column] + " ");
+			}
+			System.out.println();
+		}
+		
+		System.out.println(tempResult.length + " " + tempResult[1].length);
+		
+		System.out.println(testResult[0] + " " + testResult[1] + " " + testResult[2]);
+		
+	}
 }
